e20577d78cd40763525c479ade7eec603a814db6
LeeHakJoon <2hakjoon@gmail.com>
2021-05-21 23:53:26 +0900

    adding login page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,28 @@
+import styled from "styled-components"
 import { isLoggedInVar } from "../apollo"
+
+const Container = styled.div`
+
+`
 
 export const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={()=>isLoggedInVar(true)}>Log in</button>
-        </div>
+        <Container>
+            <div>
+                <div>
+                    <h1>Instagram</h1>
+                    <form>
+                        <input type="text" placeholder="Username"/>
+                        <input type="password" placeholder="Password"/>
+                        <input type="submit" placeholder="Log in"/>
+                    </form>
+                    <span>Or</span>
+                    <span>Log in with Facebook</span>
+                </div>
+                <div>
+                    <span>Don't have an account?</span> <a href="#">Sign up</a>
+                </div>
+            </div>
+        </Container>
     )
 }
